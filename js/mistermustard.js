5c8755516863ee4e9d35f656dc5b2855d853bd7a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-09-03 21:35:08 -0500

    [sm] major css refactor of hot dogger

--- js/mistermustard.js
+++ js/mistermustard.js
@@ -1,24 +1,23 @@
 let mustardButtons = [
-{   id: "1",
-    color: "red" },
-{   id: "2",
-    color: "rgb(249,231,33)"},
-{   id: "3",
-    color: "lime" },
-{   id: "4",
-    color: "rgb(180,45,0)"},
-{   id: "5",
-    color: "rgb(255,187,0)" },
-{   id: "6",
-    color: "turquoise"},
-{   id: "7",
-    color: "rgb(255, 255, 255)"}
-];
-
+    {   id: "1",
+        color: "red" },
+    {   id: "2",
+        color: "rgb(249,231,33)"},
+    {   id: "3",
+        color: "lime" },
+    {   id: "4",
+        color: "rgb(180,45,0)"},
+    {   id: "5",
+        color: "rgb(255,187,0)" },
+    {   id: "6",
+        color: "turquoise"},
+    {   id: "7",
+        color: "rgb(255, 255, 255)"}
+    ];
+    
 for (let i = 0; i<7; i++) {  
     document.getElementById(`color-${mustardButtons[i].id}`).addEventListener('click',(e) => {
     e.preventDefault();
-    console.log(fillColors[i]);
     document.getElementById("color-me").style.fill = mustardButtons[i].color;  
     });
 }
